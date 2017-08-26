acb60c107732d1c4921aa5e340bb9a9540483673
vikasshivankar <vikas.s.shivankar@gmail.com>
2017-08-26 23:24:51 +0530

    second commit

--- data/src/main/java/com/java/Test.java
+++ data/src/main/java/com/java/Test.java
@@ -2,8 +2,12 @@ package com.java;
 
 public class Test {
 
-	
 	public static void main(String[] args) {
+		System.out.println("hi");
+		display();
+	}
+	
+	public static void display(){
 		System.out.println("hi");
 	}
 }
